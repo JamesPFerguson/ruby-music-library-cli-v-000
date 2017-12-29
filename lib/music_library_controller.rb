b60920fefecade5bd198f9019c300f0289fe4255
James Ferguson <ncjamesferguson@gmail.com>
2017-12-29 01:39:47 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -61,7 +61,7 @@ attr_accessor :path
     end
 
     def list_songs_by_artist
-        "Please enter the name of an artist:"
+        puts "Please enter the name of an artist:"
         input = gets.strip
 
     end
