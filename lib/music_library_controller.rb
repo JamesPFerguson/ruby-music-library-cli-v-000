ef7f7aba9ab2b46610d7b43b1bb114983ec7d1a0
James Ferguson <ncjamesferguson@gmail.com>
2017-12-29 02:38:13 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -88,11 +88,10 @@ attr_accessor :path
       number = input.to_i
       if number <= Song.all.length
         song = Song.all.sort{ |a, b| a.name <=> b.name }[number - 1]
-      end
-
       if song
         puts "Playing #{song.name} by #{song.artist.name}"
     end
+  end
   end
 
 end
